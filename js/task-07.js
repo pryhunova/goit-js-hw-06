f9a3f1efffe2f83d699df94fb22b3fa268784bf0
Natalie Pryhunova <65974675+pryhunova@users.noreply.github.com>
2021-03-07 01:33:55 +0200

    Create task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,12 @@
+const filterArray = (numbers, value) => {
+    const filteredNumbers = [];
+  
+    numbers.forEach((number) => {
+      if (number > value) {
+        filteredNumbers.push(number);
+      }
+    });
+  
+    // Пиши код выше этой строки
+    return filteredNumbers;
+  }
