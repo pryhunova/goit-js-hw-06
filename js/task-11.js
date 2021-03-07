db23332aceb9b5a0e934f2cde0cc7bc62af1b5c5
Natalie Pryhunova <65974675+pryhunova@users.noreply.github.com>
2021-03-07 02:31:09 +0200

    Create task-11.js

---
+++ js/task-11.js
@@ -0,0 +1,10 @@
+const books = [
+    { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+    { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+    { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+    { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+    { title: 'Враг Божий', author: 'Бернард Корнуэлл', rating: 8.67 }
+  ];
+  // Пиши код ниже этой строки
+
+  const titles = books.map(book => book.title);
